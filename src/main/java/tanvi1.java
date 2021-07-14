7e8a4dbd8879fd53955221ae41e303d7b6f15568
tanvi-meow <tanvisharma0692@gmail.com>
2021-07-14 16:47:37 -0600

    done

---
+++ src/main/java/tanvi1.java
@@ -0,0 +1,111 @@
+//package com.company;
+
+//public class Main {
+
+  //  public static void main(String[] args) {
+  //      System.out.println("Hello World");
+  //      System.out.println("Hello World");
+   //     System.out.println("Hello World1");
+   //     System.out.println("Hello World1");
+ //   }
+//}
+
+//class Main2{
+    //public static void main(String[] args) {
+    //    System.out.println("Hello World");
+    //    System.out.println("Hello World");
+    //    System.out.println("Hello World1");
+    //    System.out.println("Hello World1");
+   // }
+//}
+//Car
+ package main.java;
+/**
+ * Automatic/Manual
+ * music system
+ * color
+ * seats
+ * steering
+ * seat belts
+ * engine
+ */
+public class tanvi1 {
+    public static void main(String[] args) {
+//        RV obj2 = new RV();
+//        obj2.name = "TanviManas";
+//        obj2.hasBed = true;
+//        obj2.hasWashroom = true;
+//        System.out.println(obj2.transmission);
+//        System.out.println(obj2.noOfSeats);
+//        System.out.println(obj2.color);
+//        System.out.println(obj2.hasEngine);
+//        System.out.println(obj2.hasMusicSystem);
+//        System.out.println(obj2.hasPowerSteering);
+//        System.out.println(obj2.hasSeatBelts);
+//        System.out.println(obj2.hasWashroom);
+//        System.out.println(obj2.hasBed);
+//        System.out.println(obj2.name);
+
+        Bike obj3 = new Bike();
+        obj3.transmission = "Manual";
+        obj3.color = "Black";
+        obj3.tank = 1;
+        obj3.noOfSeats = 2;
+        obj3.hasEngine = true;
+        obj3.hastyres = 2;
+        System.out.println(obj3.transmission);
+        System.out.println(obj3.noOfSeats);
+        System.out.println(obj3.color);
+        System.out.println(obj3.hasEngine);
+        System.out.println(obj3.tank);
+        System.out.println(obj3.hastyres);
+
+
+    }
+}
+
+/**class Mercedez {
+    String transmission = "Manual";
+    Integer noOfSeats = 5;
+}
+ */
+class Car{
+    String transmission = "Automatic/Manual";
+    boolean hasMusicSystem = true;
+    String color = "Red";
+    Integer noOfSeats = 4;
+    boolean hasPowerSteering = true;
+    boolean hasSeatBelts = true;
+    boolean hasEngine = true;
+}
+ class RV{
+    String transmission = "Automatic/Manual";
+    boolean hasMusicSystem;
+    String color;
+    Integer noOfSeats;
+    boolean hasPowerSteering;
+    boolean hasSeatBelts;
+    boolean hasEngine;
+    boolean hasBed;
+    boolean hasWashroom;
+    String name;
+}
+class Bike{
+    String transmission = "Manual";
+    String color = "Black";
+    Integer noOfSeats = 2;
+    boolean hasEngine = true;
+    Integer hastyres = 2;
+    Integer tank = 1;
+}
+
+class Tanvi{
+    String name  = "Tanvi";
+}
+
+
+
+
+//
+ //* RV
+// */
