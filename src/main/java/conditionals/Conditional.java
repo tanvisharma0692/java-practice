a3d145802a1faa268a457932b955e9a518e5c82b
tanvi-meow <tanvisharma0692@gmail.com>
2021-07-19 16:47:50 -0600

    Display garde and out of range

--- src/main/java/conditionals/Conditional.java
+++ src/main/java/conditionals/Conditional.java
@@ -4,7 +4,7 @@ public class Conditional {
 
     public static void main(String[] args) {
         //BODMAS Bracket Of Divide multiply add subtract
-        int grade = 99;
+        int grade = 76;
         //Write a program if grades are greater than 0 but less than 25 print grade 'D'
         // If grades are greater than 25 but less than 75 print 'B'
         //If grades are greater than 75 but less than or equal to 100 then print 'A'
@@ -15,14 +15,21 @@ public class Conditional {
             System.out.println("Grade B");
         } else if (grade > 75 && (grade < 100 || grade == 100)) {
             System.out.println("Grade A");
+        } else {
+            System.out.println("Out of range");
         }
+    }
+}
         //H.W
         //Write the case for Grade A, also try to display a message if grade is out of range means
         // not in the range of 0 to 100 example user enters grade 1000
-      int grade1 = 10;
-        if(grade1 > 75 && grade1 <= 100) { // 75 not included to 100
-            System.out.println("Grade A");
-        }
- //Added comment
-    }
-}
+//        int grade1 = 150;
+//        if (grade1 >= 0 && grade1 <= 100) { // 75 not included to 100
+//            System.out.println("In range");
+//        } else System.out.println("Out of range");
+//    }
+    //Added comment
+
+
+
+
